27338118afc5087a60a0c0adf95b9928e72046ad
Fabien CAGNIET <fabien.cagniet@gmail.com>
2017-12-15 15:35:12 +0100

    Ajoute un formulaire sur la page infoVisite

--- src/FC/ReservationBundle/Form/CommandeType.php
+++ src/FC/ReservationBundle/Form/CommandeType.php
@@ -2,7 +2,7 @@
 
 namespace FC\ReservationBundle\Form;
 
-use Doctrine\DBAL\Types\DateType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
