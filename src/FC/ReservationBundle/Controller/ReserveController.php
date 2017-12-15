3dbac1ce4cfc75aac47f388eb49df91de29f95a8
Fabien CAGNIET <fabien.cagniet@gmail.com>
2017-12-15 16:40:15 +0100

    Création de la page infoBillet.html.twig et du formulaire pour un ticket

--- src/FC/ReservationBundle/Controller/ReserveController.php
+++ src/FC/ReservationBundle/Controller/ReserveController.php
@@ -3,7 +3,9 @@
 namespace FC\ReservationBundle\Controller;
 
 use FC\ReservationBundle\Entity\Commande;
+use FC\ReservationBundle\Entity\Ticket;
 use FC\ReservationBundle\Form\CommandeType;
+use FC\ReservationBundle\Form\TicketType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -29,6 +31,20 @@ class ReserveController extends Controller {
             return $this->redirectToRoute('info-billet');
         } else {
             return $this->render('FCReservationBundle:Reserve:infoVisite.html.twig', array(
+                'form' => $form->createView(),
+            ));
+        }
+    }
+
+    public function infoBilletAction(Request $request){
+        $ticket = new Ticket();
+        $form = $this->get('form.factory')->create(TicketType::class, $ticket);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            return $this->redirectToRoute('info-paiement');
+        } else {
+            return $this->render('FCReservationBundle:Reserve:infoBillet.html.twig', array(
                 'form' => $form->createView(),
             ));
         }
