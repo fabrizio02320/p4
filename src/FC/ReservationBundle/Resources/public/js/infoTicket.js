3b116221ce34b93cb733606ba8f4f8c2ce21f682
Fabien CAGNIET <fabien.cagniet@gmail.com>
2018-01-29 16:24:22 +0100

    Modifie le script infoTicket.js pour pouvoir supprimer un ticket dynamiquement

--- src/FC/ReservationBundle/Resources/public/js/infoTicket.js
+++ src/FC/ReservationBundle/Resources/public/js/infoTicket.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     // récupère les tickets
     var $listeTickets = $('.tickets');
 
+    console.log($listeTickets);
     // compte  de nombre de tickets existant
     var index = $listeTickets.length;
 
@@ -17,7 +18,16 @@ $(document).ready(function(){
         addTicket(index);
     });
 
-    // todo bouton de suppression d'un ticket
+    // modifie l'attribut id du ticket pour y accéder plus facilement et renumérote le texte
+    for(var i = 0; i < index; i++){
+        $listeTickets[i].id = i + 1;
+        $listeTickets[i].querySelector('.num-ticket').textContent = i + 1;
+    }
+
+    // ajoute les boutons de suppression d'un ticket
+    for(var j = 1; j <= index; j++){
+        addBtnDelTicket($('#' + j));
+    }
 
     function addTicket(){
         // création d'un template de ticket
@@ -30,11 +40,48 @@ $(document).ready(function(){
         $ticket.attr('id', index + 1);
         $ticket.find('.num-ticket').text(index + 1);
 
-        // ajout du ticket au DOM
-        $('#add-ticket').before($ticket);
+        // ajoute le bouton supprimer
+        addBtnDelTicket($ticket);
 
+        // ajout du ticket au DOM
+        $('.tickets:last').after($ticket);
+
+        // ajoute le bouton supprimer s'il n'y avait qu'un ticket au départ
+        if(index === 1){
+            addBtnDelTicket($('.tickets:first'));
+        }
+
+        // mets à jour le nb de tickets
         index ++;
     }
 
+    function addBtnDelTicket($element){
+        // création du bouton
+        var $btnDelTicket = $('<a href="#" class="btn btn-danger del-ticket">Supprimer ce ticket</a>');
+
+        // ajout du bouton supprimer à la fin de l'élément
+        $element.append($btnDelTicket);
+
+        $btnDelTicket.click(function(e){
+            e.preventDefault();
+
+            $element.remove();
+
+            // mets à jour le nombre de tickets
+            index--;
+
+            // suite au retrait, on réindex les id et mets à jour le texte
+            var $listeTickets = $('.tickets');
+            for(var i = 0; i < index; i++){
+                $listeTickets[i].id = i + 1;
+                $listeTickets[i].querySelector('.num-ticket').textContent = i + 1;
+            }
+
+            // s'il ne reste plus qu'un ticket, retire le bouton supprimer
+            if(index === 1){
+                $('.tickets').find('.del-ticket').remove();
+            }
+        })
+    }
 
 });
